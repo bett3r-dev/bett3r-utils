a61cc368e9d9e5118be79749f174e7736352b08f
Tomás Ruiz <tomasruizr@gmail.com>
2021-03-17 01:11:13 -0300

    [add] initial functions and tests ready

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { reduce } from 'rambda';
+
+export const isError = ( error?: any ) : boolean => error instanceof Error;
+export const ensureArray = <T>( theArray?: T | T[] ): T[] => Array.isArray( theArray ) ? theArray : ( theArray != null ? [theArray] : []);
+
+export function foldMap <T>( fn: <T>(element: any) => T ): (collection: any[]) => T[];
+export function foldMap <T>( fn: <T>(element: any) => T, collection: any[]):  T[];
+export function foldMap <T>( fn: <T>(element: any) => T, collection?: any[]): T[]|Function {
+  if (!collection) return (collection: any[]) => foldMap(fn, collection)
+  return reduce(( acc, functor ) => acc.concat( functor.map ? functor.map( fn ) : fn( functor )), [], collection );
+}
+
+export function push<T>( value: any ): (list: Array<T>) => Array<T>;
+export function push<T>( value: any, list: Array<T> ): Array<T>;
+export function push<T>( value: any, list?: Array<T> ): Array<T>|Function {
+  if ( !list ) return (l: Array<T>) => push( value, l );
+  list.push( value );
+  return list;
+}
+
+export function unshift<T>( value: any ): (list: Array<T>) => Array<T>;
+export function unshift<T>( value: any, list: Array<T> ): Array<T>;
+export function unshift <T>( value: any, list?: Array<T> ): Array<T>|Function {
+  if ( !list ) return (l: Array<T>) => unshift( value, l );
+  list.unshift( value );
+  return list;
+}
+
+export function findAndPerform <T,V>( predicate: (element: any) => any ): (iterable: Iterable<T>) => V;
+export function findAndPerform <T,V>( predicate: (element: any) => any, iterable: Iterable<T> ): V;
+export function findAndPerform <T,V>( predicate: (element: any) => any, iterable?: Iterable<T> ): V|Function {
+  if ( !iterable ) return (iterable: Iterable<T>) => findAndPerform( predicate, iterable );
+  for ( const item of iterable ) {
+    const result = predicate( item );
+    if ( !result ) continue;
+    return result;
+  }
+};
+
+export default {
+  isError,
+  ensureArray,
+  foldMap,
+  push,
+  unshift,
+  findAndPerform
+}
+
---
+++ src/utils.test.ts
@@ -0,0 +1,92 @@
+import {assert} from 'chai';
+
+import {
+  isError,
+  ensureArray,
+  foldMap,
+  push,
+  unshift,
+  findAndPerform
+} from './index';
+
+describe('utils', function() {
+  describe( 'isError', function() {
+    it( 'returns true if the param passed is instance of errro', () => {
+      class X extends Error {}
+      const Y = new Error();
+      assert.isTrue( isError( new Error()));
+      assert.isTrue( isError( new X()));
+      assert.isTrue( isError( Y ));
+    });
+    it( 'returns false if the param passed is instance of errro', () => {
+      assert.isFalse( isError());
+      assert.isFalse( isError( 1 ));
+      assert.isFalse( isError( '' ));
+      assert.isFalse( isError( true ));
+      assert.isFalse( isError( null ));
+      assert.isFalse( isError({}));
+      assert.isFalse( isError([]));
+    });
+  });
+
+  describe( 'ensureArray', function() {
+    it( 'returns an array with the value passed as param if is not array', () => {
+      assert.deepEqual([], ensureArray());
+      assert.deepEqual([3],ensureArray( 3 ));
+      assert.deepEqual([10n],ensureArray( 10n ));
+      assert.deepEqual([],ensureArray( undefined ));
+      assert.deepEqual([],ensureArray( null ));
+      assert.deepEqual([false],ensureArray( false ));
+      assert.deepEqual([{}],ensureArray({}));
+      assert.deepEqual(['1'],ensureArray( '1' ));
+      assert.deepEqual(['asdf'],ensureArray( 'asdf' ));
+    });
+    it( 'returns the same value if array passed', () => {
+      assert.deepEqual([3],ensureArray([3]));
+      assert.deepEqual([10n],ensureArray([10n]));
+      assert.deepEqual([undefined],ensureArray([undefined]));
+      assert.deepEqual([null],ensureArray([null]));
+      assert.deepEqual([{}],ensureArray([{}]));
+      assert.deepEqual(['1'],ensureArray(['1']));
+      assert.deepEqual(['asdf'],ensureArray(['asdf']));
+    });
+  });
+
+  describe( 'push', function() {
+    it( 'pushes the value into an array and return the array', () =>
+      assert.deepEqual( push( 3, [ 1,2 ]) , [ 1,2,3 ]));
+    it( 'Curried pushes the value into an array and return the array', () => {
+      const fn = push( 3 );
+      assert.deepEqual( fn([ 1,2 ]) , [ 1,2,3 ]);
+    });
+  });
+
+  describe( 'foldMap', function() {
+    it( 'folds and map an array of arrays', () => {
+      const arr = [[ 1,2,3 ], [2], [ 2,3 ]];
+      assert.deepEqual( foldMap( x=>x, arr ), [ 1,2,3,2,2,3 ]);
+    });
+  });
+
+  describe( 'unshift', function() {
+    it( 'unshifts the value into an array and return the array', () =>
+      assert.deepEqual( unshift( 1, [ 2,3 ]) , [ 1,2,3 ]));
+    it( 'Curried unshifts the value into an array and return the array', () => {
+      const fn = unshift( 1 );
+      assert.deepEqual( fn([ 2,3 ]) , [ 1,2,3 ]);
+    });
+  });
+
+  describe( 'findAndPerform', function() {
+    it( 'finds an item in an array appliying a predicate, and returns the result of such apply over found item', () => {
+      const arr = [ 2, 4 ];
+      const result = findAndPerform( x => x % 2, arr );
+      assert.isUndefined( result );
+    });
+    it( 'finds an item in an array appliying a predicate, and returns the result of such apply over found item', () => {
+      const arr = [ 2, 4, 3 ];
+      const result = findAndPerform( x => x % 2, arr );
+      assert.equal( result, 1 );
+    });
+  });
+});
